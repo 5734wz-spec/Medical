8585a852819c4ef1ca8734138fcb6dfeb31b94d6
5734wz-spec <5734wz@gmail.com>
2026-04-02 18:10:42 +0800

    Add files via upload

---
+++ chatbot_graph.py
@@ -0,0 +1,31 @@
+
+from question_classifier import *
+from question_parser import *
+from answer_search import *
+
+'''问答类'''
+class ChatBotGraph:
+    def __init__(self):
+        self.classifier = QuestionClassifier()
+        self.parser = QuestionPaser()
+        self.searcher = AnswerSearcher()
+
+    def chat_main(self, sent):
+        answer = '您好，我是您的健康小助理，暂未查询到相关内容，请换种方式提问，例如：高血压的宜吃食物有哪些？'
+        res_classify = self.classifier.classify(sent)
+        if not res_classify:
+            return answe
+        res_sql = self.parser.parser_main(res_classify)
+        final_answers = self.searcher.search_main(res_sql)
+        if not final_answers:
+            return answer
+        else:
+            return '\n'.join(final_answers)
+
+if __name__ == '__main__':
+    handler = ChatBotGraph()
+    while 1:
+        question = input('用户:')
+        answer = handler.chat_main(question)
+        print('小助理:', answer)
+
